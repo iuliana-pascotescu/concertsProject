b18bb318a51cc0fe16ee654a464e3ae6a3ab49cf
Iuliana Pascotescu <iuliana.pascotescu@accesa.eu>
2021-07-30 13:21:56 +0300

    added logs

--- src/main/java/com/example/demo/controller/ControllerConcerts.java
+++ src/main/java/com/example/demo/controller/ControllerConcerts.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.ServiceConcerts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ControllerConcerts {
 
+  private static final Logger LOG = LoggerFactory.getLogger(ControllerConcerts.class);
   private final ServiceConcerts serviceConcerts;
 
   public ControllerConcerts(ServiceConcerts serviceConcerts) {
@@ -16,15 +19,18 @@ public class ControllerConcerts {
 
   @GetMapping("/concerts")
   public ResponseEntity<?> getAllConcerts() {
+    LOG.info("getAllConcert - method entered");
     return ResponseEntity.ok(serviceConcerts.getAllConcerts());
   }
 
   @RequestMapping(
       value = "/delete/{id}",
       produces = "application/json",
-      method = {RequestMethod.DELETE, RequestMethod.GET})
+      method = {RequestMethod.DELETE})
   public ResponseEntity<?> deleteConcert(@PathVariable Integer id) {
+    LOG.info("deleteConcert id={} - method entered", id);
     serviceConcerts.deleteConcertById(id);
+    LOG.info("deleteConcert id={} - method finished", id);
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
